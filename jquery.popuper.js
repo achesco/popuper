2d38638afc4ac0e1e4cd2b2469a52b03b4bcd760
Anton Lysenko <achesco@gmail.com>
2013-11-14 14:57:41 +0200

    css animation support provided

--- jquery.popuper.js
+++ jquery.popuper.js
@@ -14,13 +14,17 @@
 
 	$.fn.popuper.defaults = {
 		hiddenClass: 'g-hidden',
-		onShow: function () {},
-		onHide: function () {}
+		animatedClass: null,
+		hideDelay: 0,
+		onShow: function () {
+		},
+		onHide: function () {
+		}
 	};
 
 	var iOS = (navigator.userAgent.match(/iP(ad|hone|od)/) != null);
 
-	var PopupBlock = function(jContainer, hParam){
+	var PopupBlock = function (jContainer, hParam) {
 
 		this.hParam = $.extend({}, $.fn.popuper.defaults, hParam);
 		this.jContainer = $(jContainer);
@@ -28,78 +32,81 @@
 		this.jOpen = $(hParam.open);
 		this.jClose = this.jContainer.find(this.hParam.close);
 
-		this.hParam.container = this.jContainer; /* for events */
+		this.hParam.container = this.jContainer;
+		/* for events */
 
-		this.cbDocumentClick = $.proxy(function(event){
-			if(this.jContainer.filter(event.target).length == 0 && this.jContainer.find(event.target).length == 0){
+		this.cbDocumentClick = $.proxy(function (event) {
+			if (this.jContainer.filter(event.target).length == 0 && this.jContainer.find(event.target).length == 0) {
 				this.hide(event);
 			}
 		}, this);
 
 		this.cbDocumentKeyDown = $.proxy(this.cancel, this);
 
-		this.jOpen.click( $.proxy(this.toggle, this) );
-		this.jClose.click( $.proxy(this.hide, this) );
+		this.jOpen.click($.proxy(this.toggle, this));
+		this.jClose.click($.proxy(this.hide, this));
 	};
 
 	PopupBlock.prototype = {
 
-		toggle: function(event)
-		{
+		toggle: function (event) {
 			this.stopEvent(event);
 
-			if(this.jContainer.hasClass(this.hParam.hiddenClass))
+			if (this.jContainer.hasClass(this.hParam.hiddenClass))
 				this.show(event);
 			else
 				this.hide(event);
 		},
 
-		hide: function(event)
-		{
+		hide: function (event) {
 			if (this.hParam.onHide) this.hParam.onHide(event, this.hParam);
 
-			if(iOS){
+			if (iOS) {
 				// unbind fader handling for iOS
 				this.jFader
 					.unbind("click", this.cbDocumentClick);
 				//.unbind("touchmove");
 			}
 
-			this.jContainer.addClass(this.hParam.hiddenClass);
-			this.jFader.addClass(this.hParam.hiddenClass);
+			this.toggleState(this.jFader, 'addClass', this.hParam.hideDelay, this.hParam.animatedClass, this.hParam.hiddenClass);
+			this.toggleState(this.jContainer, 'addClass', this.hParam.hideDelay, this.hParam.animatedClass, this.hParam.hiddenClass);
 
 			$(document).unbind('click', this.cbDocumentClick);
 			$(document).unbind('keydown', this.cbDocumentKeyDown);
 		},
 
-		cancel: function(event)
-		{
+		cancel: function (event) {
 			var code = event.keyCode ? event.keyCode : event.which ? event.which : null;
 			if (code === 27) // Esc
 				this.hide(event);
 		},
 
-		show: function(event)
-		{
+		show: function (event) {
 			if (this.hParam.onShow) this.hParam.onShow(event, this.hParam);
-			if(iOS){
+			if (iOS) {
 				this.jFader.height($(window.document).height())
 					// fader handling for iOS
 					.click(this.cbDocumentClick);
 				//.bind("touchmove", function(e){ e.preventDefault() });
 			}
-			this.jFader.removeClass(this.hParam.hiddenClass);
-			this.jContainer.removeClass(this.hParam.hiddenClass);
+
+			this.toggleState(this.jFader, 'removeClass', 0, this.hParam.hiddenClass, this.hParam.animatedClass);
+			this.toggleState(this.jContainer, 'removeClass', 0, this.hParam.hiddenClass, this.hParam.animatedClass);
 
 			$(document).click(this.cbDocumentClick);
 			$(document).keydown(this.cbDocumentKeyDown);
 		},
 
-		stopEvent: function(event)
-		{
+		stopEvent: function (event) {
 			event.preventDefault();
 			event.stopPropagation();
+		},
+
+		toggleState: function (element, methodName, delay, firstClass, secondClass) {
+			element[methodName](firstClass);
+			setTimeout($.proxy(element, methodName, secondClass), delay);
 		}
+
 	};
 
 })(jQuery);
